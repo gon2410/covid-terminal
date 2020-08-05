d56c78e42a24e869bb1fb7ce3440c385d9cac57f
gon2410 <39386867+gon2410@users.noreply.github.com>
2020-08-05 17:21:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,43 @@
 
 echo 'Installing Dependencies..'
 
-sudo pacman -S python3 python-pip
+# check if python is installed
+if sudo pacman -Qi python > /dev/null; then
+    echo 'python is already installed'
+else
+    sudo pacman -S python
+fi
 
-pip install termtables
-pip install termcolor
-pip install beautifulsoup4
+# check if python-pip is installed
+if sudo pacman -Qi python-pip > /dev/null; then
+    echo 'python-pip is already installed'
+else
+    sudo pacman -S python-pip
+fi
+
+# install python libraries
+if python -c 'import termtables' > /dev/null; then
+    echo 'termtables is already installed'
+else
+    pip install termtables
+fi
+
+if python -c 'import termcolor' > /dev/null; then
+    echo 'termcolors is already installed'
+else
+    pip install termcolor
+fi
+
+if python -c 'import bs4' > /dev/null; then
+    echo 'beautifulsoup4 is already installed'
+else
+    pip install beautifulsoup4
+fi
 
 echo 'Done'
 
+# move main file to /usr/bin and make it executable
 sudo mv covid.py /usr/bin/covid
-sudo chmod +x covid
+sudo chmod +x /usr/bin/covid
 
 echo 'Succesfully Installed'
