0f1f65c4c8e83e7bdfdb544d249bb83bfa16e06c
gon2410 <39386867+gon2410@users.noreply.github.com>
2020-08-06 17:19:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,39 @@ if [[ $var == 'debian' || $var == 'ubuntu' || $var == 'mint' ]]; then
     echo 'You have a debian based distro'
     
     # check if python is installed
-    if dpkg-query -l python > /dev/null; then
-        echo 'python is already installed'
-    else
+    if ! dpkg -s python3 >/dev/null 2>&1; then
         sudo apt-get install python3
+    else
+        echo 'python3 is already installed'
     fi
 
     # check if python-pip is installed
-    if dpkg-query -l python-pip > /dev/null; then
-        echo 'python-pip is already installed'
+    if ! dpkg -s python3-pip >/dev/null 2>&1; then
+        sudo apt-get install python3-pip
     else
-        sudo apt-get install python3-pip 
+        echo 'python-pip is already installed'
     fi
 
+    # install python libraries
+    if python -c 'import termtables' > /dev/null; then
+        echo 'termtables is already installed'
+    else
+        pip3 install termtables
+    fi
+
+    if python -c 'import termcolor' > /dev/null; then
+        echo 'termcolor is already installed'
+    else
+        pip3 install termcolor
+    fi
+
+    if python -c 'import bs4' > /dev/null; then
+        echo 'beautifulsoup4 is already installed'
+    else
+        pip3 install beautifulsoup4
+    fi
+
+echo 'Done'
 elif [[ $var == 'arch' ]]; then
     echo 'You have an arch base distro'
 
@@ -37,27 +57,26 @@ elif [[ $var == 'arch' ]]; then
     else
         sudo pacman -S python-pip
     fi
-fi
 
-# install python libraries
-if python -c 'import termtables' > /dev/null; then
-    echo 'termtables is already installed'
-else
-    pip install termtables
-fi
+    # install python libraries
+    if python -c 'import termtables' > /dev/null; then
+        echo 'termtables is already installed'
+    else
+        pip install termtables
+    fi
 
-if python -c 'import termcolor' > /dev/null; then
-    echo 'termcolor is already installed'
-else
-    pip install termcolor
-fi
+    if python -c 'import termcolor' > /dev/null; then
+        echo 'termcolor is already installed'
+    else
+        pip install termcolor
+    fi
 
-if python -c 'import bs4' > /dev/null; then
-    echo 'beautifulsoup4 is already installed'
-else
-    pip install beautifulsoup4
+    if python -c 'import bs4' > /dev/null; then
+        echo 'beautifulsoup4 is already installed'
+    else
+        pip install beautifulsoup4
+    fi
 fi
-
 echo 'Done'
 
 # move main file to /usr/bin and make it executable
